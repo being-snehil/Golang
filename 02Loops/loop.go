9a9646c975dba1014bd484518b93d5b644b5d543
Snehil Verma <snehilverma607@gmail.com>
2025-10-19 22:29:45 +0530

    updated till arrays -> 1st Day

---
+++ 02Loops/loop.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+func main(){
+	// syntax for while loop in GO
+	i := 0
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
+	// syntax for  for loop in GO
+		for i := 0; i < 5; i++ {
+                    fmt.Println(i)
+          }
+
+	// syntax for infinite loop in GO
+	for {
+		println("infinite loop")
+	}
+
+	
+
+}
